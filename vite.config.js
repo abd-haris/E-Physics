077bf209967eaead6f54c5dca85539df82792a30
abd-haris <mybestcore19@gmail.com>
2025-07-01 07:51:21 +0700

    menambahkan base di konfigurasi vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   root: resolve(__dirname, "src"),
   publicDir: resolve(__dirname, "src", "public"),
+  base: "/E-Physics/",
   build: {
     outDir: resolve(__dirname, "docs"),
     emptyOutDir: true,
